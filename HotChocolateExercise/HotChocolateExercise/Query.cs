ffade2409682b792dcf62f4cf37de6d2bd98537a
GabrielReynoldsWex <gabriel.reynolds@wexinc.com>
2024-04-30 14:06:54 +1000

    Completed exercise

---
+++ HotChocolateExercise/HotChocolateExercise/Query.cs
@@ -0,0 +1,17 @@
+﻿namespace HotChocolateExercise
+{
+    public class Query
+    {
+        public Book GetBook()
+        {
+            return new Book
+            {
+                Title = "The Hobbit",
+                Author = new Author
+                {
+                    Name = "J.R.R. Tolkien"
+                }
+            };
+        }
+    }
+}
